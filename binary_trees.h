10f92c4ce2d1ebb80a0c532a537da8cc366712b8
Kevin Koech <kevinkoech@proton.me>
2023-08-02 22:53:40 +0300

    modify structure

--- binary_trees.h
+++ binary_trees.h
@@ -14,13 +14,15 @@
  * @right: Pointer to the right child node
  */
 
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
 
 /* typedefs */
 typedef struct binary_tree_s binary_tree_t;
