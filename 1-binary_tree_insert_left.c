44a9bf5ca9cfd37830b6071446d937d5d9d12a26
Kevin Koech <kevinkoech@proton.me>
2023-08-03 10:30:37 +0300

    git push

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -34,7 +34,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left != NULL)
 	{
-		parent->left->parent = new_node;
+		new_node->left->parent = new_node;
 	}
 
 	parent->left = new_node;
