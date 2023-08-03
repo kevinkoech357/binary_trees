e2f7b65b74bca369d502b5e11fbd3cc60784eb41
Kevin Koech <kevinkoech@proton.me>
2023-08-03 10:23:09 +0300

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,7 +32,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
-		parent->left->parent = new_node;
+		new_node->left->parent = new_node;
 
 	parent->left = new_node;
 
