e8af00c22b731ffa66df307f487e069b12631400
Kevin Koech <kevinkoech@proton.me>
2023-08-03 10:28:24 +0300

    add a left child node to another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,12 +16,13 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
 		return (NULL);
+
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -32,9 +33,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
-		new_node->left->parent = new_node;
+	{
+		parent->left->parent = new_node;
+	}
 
 	parent->left = new_node;
-
+	
 	return (new_node);
 }
